4b7e3670112bfeded7e1c5000c5a357a8854f1c3
Josh <josh.cu@gmail.com>
2024-04-29 16:20:15 -0500

    Update Dockerfile.t-route and build.sh for versioning

---
+++ docker/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+VERSION="0.0.1"
+
+docker buildx build --platform linux/amd64 \
+  --build-arg VERSION=$VERSION \
+  --tag ngen-deps:ngen-deps-$VERSION --tag ngen-deps:latest --file Dockerfile.ngen-deps . \
+# && docker buildx build --platform linux/amd64 \
+#   --build-arg VERSION=$VERSION \
+#   --tag t-route:t-route-$VERSION --tag t-route:latest --file Dockerfile.t-route .
+# && docker buildx build --platform linux/amd64 \
+#   --build-arg VERSION=$VERSION \
+#   --tag ngen:ngen-$VERSION --tag ngen:latest --file Dockerfile.ngen . \
+# && docker buildx build --platform linux/amd64 \
+#   --build-arg VERSION=$VERSION \
+#   --tag ngiab:$VERSION --tag ngiab:latest .
+
+# docker buildx build --platform linux/amd64,linux/arm64 \
+#   --build-arg VERSION=$VERSION \
+#   --tag ngen-deps:ngen-deps-$VERSION --tag ngen-deps:latest --file Dockerfile.ngen-deps . \
+# && docker buildx build --platform linux/amd64,linux/arm64 \
+#   --build-arg VERSION=$VERSION \
+#   --tag t-route:t-route-$VERSION --tag t-route:latest --file Dockerfile.t-route .
+# && docker buildx build --platform linux/amd64,linux/arm64 \
+#   --build-arg VERSION=$VERSION \
+#   --tag ngen:ngen-$VERSION --tag ngen:latest --file Dockerfile.ngen . \
+# && docker buildx build --platform linux/amd64,linux/arm64 \
+#   --build-arg VERSION=$VERSION \
+#   --tag ngiab:$VERSION --tag ngiab:latest .
